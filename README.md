6284740df3230eccd1e3273366a12586a1f1c161
Halil Can CALISKAN <61836866+halilcancaliskan@users.noreply.github.com>
2024-04-12 18:26:11 +0200

    readme + commentary

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-ok
+Projet de pagination de données avec MongoDB
+Ce projet est une application Node.js qui utilise MongoDB pour paginer les données d'une collection en fonction des paramètres de pagination fournis par l'utilisateur.
+
+Installez les dépendances nécessaires :
+```npm install```
+
+Utilisation
+Exécutez l'application en spécifiant les paramètres de pagination dans la ligne de commande. Par exemple :
+```node app.js title 5 10 asc```
+
+Dans cet exemple, les données seront paginées par le champ "title", affichant la 5eme page avec 10 documents par page, triés par ordre ascendant.
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const { MongoClient } = require('mongodb');
+// Module qui récupère les arguments de la ligne de commande
 const argv = require('yargs').argv;
 
+// Connexion à la base de données MongoDB
 const mongoUrl = 'mongodb+srv://hcaliskan:hcaliskan@iot.i2m1jq2.mongodb.net';
 const dbName = 'sample_mflix';
 
@@ -9,7 +11,7 @@ let collection;
 
 MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(client => {
-        console.log('Connecté à MongoDB');
+        console.log('-> Connecté à MongoDB <-');
         db = client.db(dbName);
         collection = db.collection('movies');
         paginateData();
@@ -19,12 +21,10 @@ MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true 
 // Fonction pour paginer les données
 async function paginateData() {
     try {
-        const field = argv._[0]; // Champ sur lequel trier les données
-        const pageNumber = parseInt(argv._[1]) || 1; // Numéro de page par défaut
-        const pageSize = parseInt(argv._[2]) || 10; // Nombre de documents par page par défaut
-
-        // Calcul du nombre de documents à ignorer pour la pagination
-        const skip = (pageNumber - 1) * pageSize;
+        const field = argv._[0]; // Champ sur lequel trier les données, récupérer le premier argument de la ligne de commande
+        const pageNumber = parseInt(argv._[1]) || 1; // Numéro de page par défaut, récupérer le deuxième argument de la ligne de commande
+        const pageSize = parseInt(argv._[2]) || 10; // Nombre de documents par page par défaut, récupérer le troisième argument de la ligne de commande
+        const skip = (pageNumber - 1) * pageSize; // Calcul du nombre de documents à ignorer pour la pagination
 
         // Requête pour récupérer les données de la collection 'movies', triées par le champ spécifié dans la commande
         const cursor = collection.find({})
@@ -32,14 +32,9 @@ async function paginateData() {
             .skip(skip)
             .limit(pageSize);
 
-        // Conversion en tableau
-        const docs = await cursor.toArray();
-
-        // Récupération du nombre total de documents dans la collection
-        const totalDocs = await collection.countDocuments({});
-
-        // Vérification de la présence d'une page suivante
-        const hasNextPage = skip + pageSize < totalDocs;
+        const docs = await cursor.toArray(); // Conversion en tableau
+        const totalDocs = await collection.countDocuments({}); // Récupération du nombre total de documents dans la collection
+        const hasNextPage = skip + pageSize < totalDocs; // Vérification de la présence d'une page suivante
 
         // Création de l'objet JSON de réponse
         const result = {
@@ -50,7 +45,7 @@ async function paginateData() {
             hasNextPage
         };
 
-        // Affichage ou retour du résultat
+        // Affichage du résultat
         console.log(result);
     } catch (error) {
         console.error('Erreur de pagination:', error);
